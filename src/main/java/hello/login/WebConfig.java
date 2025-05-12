0abe13d2ef2cb186cec2d8eb1ae37b75e2f5d7fc
Bmijin <bmj13465@jtts.co.kr>
2025-05-12 10:00:01 +0900

    스프링 인터셉터 - 인증 체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,8 +3,8 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,8 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
-                .addPathPatterns("/**")                                                     // 모든 경로에 다 적용되지만,
-                .excludePathPatterns("/css/**", "/*.ico", "/error", "/login", "/logout");    // 이 경로들은 인터셉터 적용하지 마.
+                .addPathPatterns("/**")                                 // 모든 경로에 다 적용되지만,
+                .excludePathPatterns("/css/**", "/*.ico", "/error");    // 이 경로들은 인터셉터 적용하지 마.
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "logout", "/css/**", "/*.ico", "/error");
     }
 
 //    @Bean
@@ -32,7 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
